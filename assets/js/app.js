6f674b5e31d194cd49218d18b214526ec9c00b4b
Alex Riner <rineralexander@gmail.com>
2020-11-30 14:10:16 -0600

    added dots

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,29 +19,42 @@ var chartGroup = svg.append("g")
 //read data and make graph
 d3.csv("assets/data/data.csv").then(function(data) {
 
+    //change values from string to numbers
     data.forEach(function(entry) {
         entry.poverty = +entry.poverty;
         entry.obesity = +entry.obesity;
     });
     console.log(data[0].poverty);
-
+    
+    // set up x axis
     var xScale = d3.scaleLinear()
     .domain(d3.extent(data, entry => entry.poverty))
     .range([0, chartWidth]);
-    
-    var yScale = d3.scaleLinear()
-    .domain(d3.extent(data, entry => entry.obesity))
-    .range([chartHeight, 0]);
 
     var bottomAxis = d3.axisBottom(xScale);
     chartGroup.append("g")
     .classed("axis", true)
     .attr("transform", `translate(0, ${chartHeight})`)
     .call(bottomAxis);
+    
+    // set up y axis
+    var yScale = d3.scaleLinear()
+    .domain(d3.extent(data, entry => entry.obesity))
+    .range([chartHeight, 0]);
 
     var leftAxis = d3.axisLeft(yScale);
     chartGroup.append("g")
     .classed("axis", true)
     .call(leftAxis);
+
+    svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (entry) { return xScale(entry.poverty); } )
+      .attr("cy", function (entry) { return yScale(entry.obesity); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
 
 });
